42c3c3564207834604a0c0aa8397dd7cfe75ab60
Ronald Frota <65458407+Danzai-Senpai@users.noreply.github.com>
2023-01-27 13:37:11 -0300

    Conditional Study
    Basic application of conditionals in c++.

---
+++ conditional.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 3;
+    int b = 2;
+
+    if (a<b) {
+
+        cout << "a e menor que b" << endl;
+
+    } else if (a>b) {
+        
+        cout << "a e maior que b" << endl;
+
+    } else{
+
+        cout << "a e b são iguais" << endl;
+
+    }
+
+    return 0;
+
+}
