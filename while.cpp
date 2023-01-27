c7ac0a92d6728286343716c1be1a25c0fea2c8fa
Ronald Frota <65458407+Danzai-Senpai@users.noreply.github.com>
2023-01-27 14:07:44 -0300

    Study of Loops
    Study of loops of type while and type for.

---
+++ while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1 = 1;
+
+    while (num1 < 10) {
+
+        cout << "Numero: " << num1 << endl;
+        num1++;
+
+    }
+
+    return 0;
+
+}
