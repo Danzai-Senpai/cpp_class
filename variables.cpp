9f2a531d93a9909a2a0c97ad8aba71ea5cbe385e
Ronald Frota <65458407+Danzai-Senpai@users.noreply.github.com>
2023-01-27 10:26:20 -0300

    Att_Variables
    Add more tests with different variable types.

--- variables.cpp
+++ variables.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    int vida;
-    vida = 5;
-    cout<<vida<<endl;
+
+    int life;
+    float energy;
+    bool playerStayAlive;
+    string playerName;
+
+    life = 5;
+    energy = 2.75f;
+    playerStayAlive = true;
+    playerName = "John";
+
+    cout<<life<<endl;
+    cout<<energy<<endl;
+    cout<<playerStayAlive<<endl;
+    cout<<playerName<<endl;
+
     return 0;
+
 }
